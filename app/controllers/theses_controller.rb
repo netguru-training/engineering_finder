85e3369904d78202a731abb12bb833e180beb9ce
Mossie93 <rmossakowski@gmail.com>
2014-11-15 14:47:47 +0100

    Added creating, showing and updating thesis_controller functionalities

--- app/controllers/theses_controller.rb
+++ app/controllers/theses_controller.rb
@@ -1,4 +1,8 @@
 class ThesesController < ApplicationController
+  expose :theses
+  expose(:thesis, attributes: :thesis_params)
+  expose :user
+
   def index
 
   end
@@ -8,17 +12,23 @@ class ThesesController < ApplicationController
   end
 
   def new
-    @thesis = Thesis.new()
+    thesis = Thesis.new()
   end
 
   def create
-    @thesis = Thesis.new(thesis_params)
-    @thesis.save()
+    thesis = Thesis.new(thesis_params)
+    thesis.user_id = current_user.id
+    thesis.save()
     redirect_to root_path
   end
 
-  def delete
+  def edit
 
+  end
+
+  def update
+    thesis.update(thesis_params)
+    redirect_to thesis_path(thesis.id)
   end
 
 
@@ -26,6 +36,6 @@ class ThesesController < ApplicationController
   private
 
   def thesis_params
-    params.require(:thesis).permit(:title, :description, :university)
+    params.require(:thesis).permit(:title, :description, :university, :user_id)
   end
 end
