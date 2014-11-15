f0465e297de8670e34126c4ff9c57df8da211077
kdabrowski <biz@op.pl>
2014-11-15 17:48:24 +0100

    Added admin helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def is_admin?
+   unless current_user && :user_signed_in? && current_user.admin?
+      flash[:error] = "You must be admin logged in to access this section"
+      redirect_to  new_user_session_path
+      return true
+    end
+    true
+  end
+
 end
