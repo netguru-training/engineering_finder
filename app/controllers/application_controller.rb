06706e84403e0b140c718482801391be8cb74483
kdabrowski <biz@op.pl>
2014-11-15 15:51:41 +0100

    Quickfix for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit :name, :lastname, :firstname, :email, :password, :password_confirmation
-    end
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit :name, :lastname, :firstname, :email, :password, :password_confirmation
+      end
+  end
 end
