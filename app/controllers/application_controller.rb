d1d371c414a2c630cf9e7985c92d963b9ae75867
kdabrowski <biz@op.pl>
2014-11-15 16:50:48 +0100

    Fixed devise bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include ActiveModel::ForbiddenAttributesProtection
+
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
